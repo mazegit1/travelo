927326c0977b7b16d379cb598fb28089405c647a
mazegit1 <editor.azer@gmail.com>
2025-01-17 20:15:28 +0400

    wagw

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween:80,
+    loop: true,
+  autoplay:{
+    delay:500,
+  }
+  
+  });
